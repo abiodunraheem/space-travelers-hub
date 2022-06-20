55bebe18c72b57457bcd9bad8b24b64e2e06e32b
bizip <bizip04@gmail.com>
2022-06-20 21:26:10 +0200

    Add Router and refactor it

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const propTypes = {};
+
+const defaultProps = {};
+
+/**
+ *
+ */
+const Home = () => (
+  <div>
+    <h1>Welcome to the home page</h1>
+  </div>
+);
+
+Home.propTypes = propTypes;
+Home.defaultProps = defaultProps;
+// #endregion
+
+export default Home;
