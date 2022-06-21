abc2fd9aeff02e7a32d921d2cc0f5a49b3aecaa0
abiodunraheem <raheemabiodun23@gmail.com>
2022-06-21 14:04:27 +0100

    Update myprofile file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/layout/Navbar';
+import Myprofile from './components/myprofile/Myprofile';
 import AllRouter from './router/Router';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Myprofile />
       <AllRouter />
     </div>
   );
---
+++ src/components/myprofile/Myprofile.js
@@ -0,0 +1,34 @@
+import { useSelector } from 'react-redux';
+import './myprofile.css';
+
+const Myprofile = () => {
+  const rockets = useSelector((state) => state.RocketsReducer);
+  const missions = useSelector((state) => state.missionReducer);
+
+  return (
+    <div className="myProfile">
+      <section className="my-rockets">
+        <h2>My Mission</h2>
+        <ul className="reservedCont">
+          { missions.map((mission) => (mission.reserved === true ? (
+            <li className="reservedItem" key={mission.id}>
+              {' '}
+              {mission.name}
+              {' '}
+            </li>
+          ) : null))}
+        </ul>
+      </section>
+      <section className="my-rockets">
+        <h2>My Rockets</h2>
+        <ul className="reservedCont">
+          {rockets.map((rocket) => (rocket.reserved === true
+            ? (<li className="reservedItem" key={rocket.id}>{rocket.name}</li>) : null
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default Myprofile;
