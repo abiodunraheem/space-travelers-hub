a34aa98dc42edf5494b6c6d96c2a5cd02996c151
bizip <bizip04@gmail.com>
2022-06-21 10:48:06 +0200

    Add Navbar

---
+++ src/components/Layouts/NavBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+// #region constants
+
+// #endregion
+
+// #region styled-components
+
+// #endregion
+
+// #region functions
+
+// #endregion
+
+// #region component
+const propTypes = {};
+
+const defaultProps = {};
+
+/**
+ * 
+ */
+const NavBar = () => {
+    return <div></div>;
+}
+
+NavBar.propTypes = propTypes;
+NavBar.defaultProps = defaultProps;
+// #endregion
+
+export default NavBar;
