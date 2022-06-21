b3a2d4e75387f5dcd2632360b14b385e801fc1cd
abiodunraheem <raheemabiodun23@gmail.com>
2022-06-21 10:23:08 +0100

    Add navbar and header to project

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav className="nav-bar">
+    <div className="header">
+      <img src="" alt="" />
+      <h1>Space Travellers Hub</h1>
+    </div>
+  </nav>
+);
+
+export default Navbar;
