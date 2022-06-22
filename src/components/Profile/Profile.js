aa087f805fac3ef83d7f1c5562ba797528cdf99f
bizip <bizip04@gmail.com>
2022-06-22 18:40:01 +0200

    Add profile Page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Spinner } from 'react-spinners-css';
+// import { Spinner } from 'react-spinners-css';
 import style from './Profile.module.css';
 import { displayMissionFunction } from '../../redux/mission/Mission';
 
@@ -10,23 +10,30 @@ const defaultProps = {};
 
 const Profile = () => {
   const dispatch = useDispatch();
-  const missionList = useSelector((state) => state.missionReducer);
+  const missionList = useSelector((state) => state.missionReducer.response);
   useEffect(() => {
     dispatch(displayMissionFunction());
   }, []);
   return (
 
     <div className={style.myprofile}>
-      <ul className={style.myrocket}>
-        <li><p>grinade</p></li>
-        <li><p>grinade</p></li>
-        <li><p>grinade</p></li>
-      </ul>
-      <ul className={style.myrocket}>
-        <li><p>grinade</p></li>
-        <li><p>grinade</p></li>
-        <li><p>grinade</p></li>
-      </ul>
+      <div className={style.mymission}>
+        <h1>My Mission</h1>
+        <ul className={style.table}>
+          {missionList.map((el) => (
+            <li key={el.mission_id}><p>{el.mission_name}</p></li>
+          ))}
+
+          <li><p>grinade</p></li>
+        </ul>
+      </div>
+      <div className={style.myrocket}>
+        <h1>My Mission</h1>
+        <ul className={style.table}>
+          <li><p>grinade</p></li>
+          <li><p>grinade</p></li>
+        </ul>
+      </div>
     </div>
 
   );
