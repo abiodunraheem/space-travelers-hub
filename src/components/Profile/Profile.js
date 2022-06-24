ba95bb7e648adba31a87d2e546d304f3e531f290
bizip <bizip04@gmail.com>
2022-06-24 12:36:34 +0200

    Sove conflict

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -10,8 +10,9 @@ const defaultProps = {};
 
 const Profile = () => {
   const dispatch = useDispatch();
-  const missionList = useSelector((state) => state.missionReducer);
-  console.log(missionList);
+  const missionList = useSelector((state) => state.missionReducer.response);
+  const rocketsList = useSelector((state) => state.rocketReducer.response);
+  console.log(rocketsList)
   useEffect(() => {
     dispatch(displayMissionFunction());
   }, []);
@@ -31,8 +32,9 @@ const Profile = () => {
       <div className={style.myrocket}>
         <h1>My Mission</h1>
         <ul className={style.table}>
-          <li><p>grinade</p></li>
-          <li><p>grinade</p></li>
+          {rocketsList.map((el) => (
+            <li key={el.rockets_id}><p>{el.rockets_name}</p></li>
+          ))}
         </ul>
       </div>
     </div>
