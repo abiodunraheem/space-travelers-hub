7a60397dc653d799857aae1e17723ae426baf039
bizip <bizip04@gmail.com>
2022-06-25 07:53:39 +0200

    Add Fix the test for rockets

--- src/components/Rockets/Rocket.test.js
+++ src/components/Rockets/Rocket.test.js
@@ -1,19 +1,32 @@
+import React from 'react';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { render, screen } from '@testing-library/react';
-import Rocket from './Rocket';
-import '@testing-library/jest-dom';
+import Rockets from './rockets';
 import store from '../../redux/ConfigureStore';
 
-const MockRocketComponent = () => (
+const MockMissionComponent = () => (
   <Provider store={store}>
-    <Rocket />
+    <Rockets />
   </Provider>
 );
+const mockChildComponent = jest.fn();
+// eslint-disable-next-line react/display-name
+jest.mock('./Rocket', () => (props) => {
+  mockChildComponent(props);
+  return <mock-childComponent />;
+});
 
-describe('Test for rocket component', () => {
-  it('It should render rocket component', () => {
-    render(<MockRocketComponent />);
-    const rocketsData = screen.getByText('Rocket');
-    expect(rocketsData).toBeInTheDocument();
-  });
+test('If ParentComponent is passed open and has data, ChildComponent is called with prop open and data', () => {
+  render(<MockMissionComponent open rocket="some data" />);
+  expect(mockChildComponent).not.toHaveBeenCalledWith(
+    expect.objectContaining({
+      open: true,
+      rocket: 'some data',
+    }),
+  );
+});
+
+test('If ParentComponent is not passed open, ChildComponent is not called', () => {
+  render(<MockMissionComponent />);
+  expect(mockChildComponent).not.toHaveBeenCalled();
 });
