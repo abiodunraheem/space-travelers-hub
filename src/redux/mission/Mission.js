6cf1aaa2dedd29fdbc6c05bc380ae5b97bd3586a
abiodunraheem <raheemabiodun23@gmail.com>
2022-10-21 21:21:10 +0100

    Fetch mission from api

--- src/redux/mission/Mission.js
+++ src/redux/mission/Mission.js
@@ -9,3 +9,24 @@ const MISSION_STATUS = 'bookstore/mission/MISSION_STATUS';
 const url = 'https://api.spacexdata.com/v3/missions';
 
 // defining initial state
+const initialState = [];
+
+// Action creattor
+export const missionStatusFunction = (id) => (dispatch) => {
+  try {
+    return dispatch({ type: MISSION_STATUS, payload: id });
+  } catch (err) {
+    return err;
+  }
+};
+
+// Action creator function that will display data once it is dispatched
+export const displayMissionFunction = () => async (dispatch) => {
+  try {
+    const res = await axios.get(url);
+    return dispatch({ type: DISPLAY_MISSION, payload: res.data });
+  } catch (err) {
+    return err;
+  }
+};
+
