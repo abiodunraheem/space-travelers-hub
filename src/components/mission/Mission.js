26756042897f6a053192e7d9ab8e32d92d0ad45c
abiodunraheem <raheemabiodun23@gmail.com>
2022-10-21 21:31:07 +0100

    Refactor mission.js file

--- src/components/mission/Mission.js
+++ src/components/mission/Mission.js
@@ -21,27 +21,7 @@ const Mission = () => {
   return (
     <div>
       {missionList.isLoading && <div className={style.spinner}><Spinner color="#ffff" /></div>}
-      <div className={style.table}>
-        <div className={style.tablerow}>
-          <div className={style.tablehead}>Mission</div>
-          <div className={style.tablehead}>Description</div>
-          <div className={style.tablehead}>Status</div>
-          <div className={style.tablehead}> </div>
-        </div>
-        {missionList.map((el, index) => (
-          <div className={`${style.tablerow}`} style={index % 2 === 0 ? { backgroundColor: '#c0c0c07e' } : { backgroundColor: 'white' }} key={el.mission_id}>
-            <div className={style.tablecell}><p className={style.btn}>{el.mission_name}</p></div>
-            <div className={style.tablecell}>
-              <p className={style.desk}>
-                {el.description}
-              </p>
-            </div>
-            <div className={`${style.tablecell} ${style.pad}`}>
-              <button className={`${el.active ? style.activeMember : style.notMember} ${style.btn}`} type="button">{el.active ? 'Active Member' : 'Not a member'}</button>
-            </div>
-            <div className={style.tablecell}><button className={`${el.active ? style.activeMemberbtn : style.notactiveMemberBtn} ${style.btn}`} onClick={handelIsMember} id={el.mission_id} type="button">{el.active ? 'Cancel mission' : 'Join mission'}</button></div>
-
-          </div>
+      
         ))}
 
       </div>
