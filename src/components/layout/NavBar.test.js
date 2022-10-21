d742942e86a10f6cb9ee7fdadfeae99dd9c08172
abiodunraheem <raheemabiodun23@gmail.com>
2022-10-21 21:52:35 +0100

    Refactor navbar test

--- src/components/layout/NavBar.test.js
+++ src/components/layout/NavBar.test.js
@@ -3,3 +3,11 @@ import renderer from 'react-test-renderer';
 import { HashRouter as Router } from 'react-router-dom';
 import Navbar from './Navbar';
 
+describe('Test for the navigation bar section', () => {
+  it('It should render the navigation bar', () => {
+    const nav = renderer
+      .create(<Router><Navbar /></Router>)
+      .toJSON();
+    expect(nav).toMatchSnapshot();
+  });
+});
