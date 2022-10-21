3eac880e9cae93076b164f61c393c491687a87c0
abiodunraheem <raheemabiodun23@gmail.com>
2022-10-21 21:35:48 +0100

    Write test for mission component

--- src/components/mission/Mission.test.js
+++ src/components/mission/Mission.test.js
@@ -4,3 +4,16 @@ import Mission from './Mission';
 import '@testing-library/jest-dom';
 import store from '../../redux/ConfigureStore';
 
+const MockMissionComponent = () => (
+  <Provider store={store}>
+    <Mission />
+  </Provider>
+);
+
+describe('Test for mission component', () => {
+  it('It should render mission component', () => {
+    render(<MockMissionComponent />);
+    const missionsData = screen.getByText('Mission');
+    expect(missionsData).toBeInTheDocument();
+  });
+});
